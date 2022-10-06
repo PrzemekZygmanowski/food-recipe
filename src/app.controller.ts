801686939204df49861ae5515a95317b49202700
przemyslaw.zygmanowski <przemyslaw.zygmanowski@makolab.com>
2022-10-06 22:45:55 +0200

    first methods in controler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller()
 export class AppController {
@@ -6,8 +14,24 @@ export class AppController {
   getHello(): string {
     return 'hello!';
   }
+
   @Get('/user')
   getSample() {
     return { name: 'PRzemek' };
+  }
+
+  @Post('')
+  createFruit(@Body() fruit: { name: string }) {
+    return fruit;
+  }
+
+  @Put('')
+  updateFruit(@Body() fruit: { name: string }) {
+    return fruit;
+  }
+
+  @Delete(':fruitId')
+  deleteFruit(@Param('fruitId') fruitId: string) {
+    return { fruitId };
   }
 }
