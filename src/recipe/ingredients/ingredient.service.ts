d0b855e39bb57bb51c992b3ddbbb7e7374190aee
przemyslaw.zygmanowski <przemyslaw.zygmanowski@makolab.com>
2022-10-18 21:31:44 +0200

    create repository

--- src/recipe/ingredients/ingredient.service.ts
+++ src/recipe/ingredients/ingredient.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Ingredient } from './ingredient.entity';
+import { IngredientRepository } from './ingredient.repository';
 
 @Injectable()
-export class IngredientService {}
+export class IngredientService {
+  constructor(private readonly ingredientRepository: IngredientRepository) {}
+
+  async findOne(id: number): Promise<Ingredient> {
+    const ingredient = await this.ingredientRepository.findById(id);
+    if (!ingredient) {
+      throw new NotFoundException(`Ingredient with id ${id} not found`);
+    }
+    return ingredient;
+  }
+}
